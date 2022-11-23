34357fdd8be2548e1fcb8903f6a82cef9bafd8b4
1234bharathi <bharathi.selvakumar@searce.com>
2022-11-23 12:56:54 +0530

    testing_godoc.org

---
+++ testing/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	// "./testing/mymathtwo"
+	// "D:/GOPROGRAMS/GOLANG-NINJA-EXERCISES/testing/mymathtwo"
+	"bharathi/testing/mymathtwo"
+	// "github.com/GoesToEleven/go-programming/code_samples/007-documentation/01/mymath"
+)
+
+func main() {
+	fmt.Println("2 + 3 =", mymathtwo.Sum(2, 3))
+	fmt.Println("4 + 7 =", mymathtwo.Sum(4, 7))
+	fmt.Println("5 + 9 =", mymathtwo.Sum(5, 9))
+}
